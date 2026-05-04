77648018079a926140f11933121deb9c9c078a4e
timeread11 <timeread11@gmail.com>
2026-05-04 13:14:29 +0300

    Add files via upload

---
+++ app/src/main/java/ru/wbankirruru/app/MainActivity.java
@@ -0,0 +1,38 @@
+package ru.wbankirruru.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://wbankir.ru");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
